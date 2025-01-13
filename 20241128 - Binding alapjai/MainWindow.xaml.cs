246aa1e6251b4897a2eef2f7304f300b3bcd31e3
AsdCat19 <113968415+AsdCat@users.noreply.github.com>
2025-01-13 22:28:38 +0100

    Add files via upload

---
+++ 20241128 - Binding alapjai/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20241128___Binding_alapjai
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        #region Szám1
+        public int Szám1 { get; set; }
+
+        private void Szám1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this.Szám1.ToString());
+        }
+
+        private void Hozzáadás_Click(object sender, RoutedEventArgs e)
+        {
+            this.Szám1++;
+            this.Szám1Textbox.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+        }
+        #endregion
+
+        #region Szám2
+        private int szám2 = 8;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int Szám2 { get => szám2; set => szám2 = value; }
+
+        private void Szám2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this.Szám2.ToString());
+        }
+
+        private void Hozzáadás2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Szám2++;
+            this.Frissítés();
+        }
+
+        private void Frissítés()
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Szám2"));
+        }
+        
+
+        private void Szám2Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.Frissítés();
+        }
+
+        private void Szám2_2Textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.Frissítés();
+        }
+        #endregion
+
+        #region NévEmail
+
+        public class User:INotifyPropertyChanged
+        {
+            private string name = "";
+            public string Name {
+                get => this.name;
+                set {
+                    this.name = value;
+                    if (this.PropertyChanged != null)
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+            private string email = "";
+            public string Email
+            {
+                get => this.email;
+                set
+                {
+                    this.email = value;
+                    if (this.PropertyChanged != null)
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+                }
+            }
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+        }
+
+        private User u1 = new User()
+        {
+            Name = "Kovács János",
+            Email = "kovacs@janos.hu"
+        };
+        public User U1
+        {
+            get => u1;
+            set => u1 = value;
+        }
+
+        private void UserKiiratás_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this.U1.Name + ": " + this.U1.Email);
+        }
+
+        private void NévMegváltoztatása_Click(object sender, RoutedEventArgs e)
+        {
+            this.U1.Name  = "Nagy János";
+            this.U1.Email = "nagy@janos.eu";
+        }
+        #endregion
+
+    }
+}
---
+++ 20241205 - Adatbáziskezelés 1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20241205___Adatbáziskezelés_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "server=localhost;uid=root;pwd=;database=konyvtarak";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM megyek";
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string megyenév = reader.GetString("megyeNev");
+                MessageBox.Show(megyenév);
+            }
+            conn.Close();
+        }
+    }
+}
